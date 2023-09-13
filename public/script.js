621ea41e8957291a8fa993291e52fc727453d8c7
Demetris <perdikos.jimmy@gmail.com>
2023-09-13 14:34:57 -0400

    Initial commit with backend and frontend setup

--- public/script.js
+++ public/script.js
@@ -1,7 +1,9 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    fetch('/health-check')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('content').textContent = "From Health Check: " + data;
-        });
-});
+fetch('http://localhost:3000/health-check')
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('health-check').textContent = data.message;
+    })
+    .catch(error => {
+        console.error('Error fetching health check:', error);
+        document.getElementById('health-check').textContent = 'Error fetching data from backend.';
+    });
